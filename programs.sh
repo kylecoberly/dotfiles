79c0fa6d55cfc981c2e8d558db60c6a7399d8d47
Kyle Coberly <kyle.coberly@gmail.com>
2022-07-14 06:02:49 +0000

    Flatten structure

---
+++ programs.sh
@@ -0,0 +1,5 @@
+sudo apt update && \
+  sudo apt install -y iproute2 xclip tree nmap build-essential \
+  ranger tmux fonts-powerline \
+  neovim python-neovim python3-neovim \
+  docker-ce docker-ce-cli
