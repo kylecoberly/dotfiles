37979ddafae63abbf0cadd45db67509439adeaa5
Kyle Coberly <kyle.coberly@gmail.com>
2022-07-14 11:19:54 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ fi
 ln -sf ./coberly-gruvbox.zsh-theme ~/.oh-my-zsh/custom/themes
 
 #Remove old dotfiles and replace them for the host machine
-DOTFILES=(.gitconfig .bashrc .zshrc .aliases init.vim .tmux.conf, .tmux.conf.local) 
-for dotfile in $(echo ${DOTFILES[*]});
-do
-  sudo rm -rf ~/$(echo $dotfile)
-  ln -sf $(echo $dotfile) ~/$(echo $dotfile)
-done
+#DOTFILES=(.gitconfig .bashrc .zshrc .aliases init.vim .tmux.conf, .tmux.conf.local) 
+#for dotfile in $(echo ${DOTFILES[*]});
+#do
+#  sudo rm -rf ~/$(echo $dotfile)
+#  ln -sf $(echo $dotfile) ~/$(echo $dotfile)
+#done
