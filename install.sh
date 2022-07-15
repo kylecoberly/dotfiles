0cbe9b2017f9ae497b2e7d7f66cb95fb1886ab9f
Kyle Coberly <kyle.coberly@gmail.com>
2022-07-15 13:43:39 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # Shell
-#OHMYZSH_DIR="$HOME/.oh-my-zsh"
-#if [ ! -d "${OHMYZSH_DIR}" ]
-#then
-  #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#fi
-#ln -sf ./coberly-gruvbox.zsh-theme $HOME/.oh-my-zsh/custom/themes
-# chsh -s /bin/zsh $USERNAME
+OHMYZSH_DIR="$HOME/.oh-my-zsh"
+if [ ! -d "${OHMYZSH_DIR}" ]
+then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 # Programs
 
@@ -19,15 +17,14 @@ sudo apt install -y --no-install-recommends ranger bat htop ncdu nmap tldr tree 
 
 ## Development
 
-sudo apt install -y --no-install-recomends tmux neovim
+sudo apt install -y --no-install-recommends tmux neovim
 
 ## Link Files
-
+ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/coberly-gruvbox.zsh-theme $HOME/.oh-my-zsh/custom/themes
 DOTFILES=(.gitconfig .zshrc .aliases init.vim .tmux.conf, .tmux.conf.local) 
 for dotfile in $(echo ${DOTFILES[*]});
 do
   ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/$(echo $dotfile) $HOME/$(echo $dotfile)
 done
 
-pwd | wall
 echo "Environment setup complete" | wall
