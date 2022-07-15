25bdb95b63bdf2bf5152c0da4d3e777fbc6541bb
Kyle Coberly <kyle.coberly@gmail.com>
2022-07-15 06:03:35 +0000

    Update installation

--- install.sh
+++ install.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-# Install oh-my-zsh and link the theme
+# Shell
 OHMYZSH_DIR="$HOME/.oh-my-zsh"
 if [ ! -d "${OHMYZSH_DIR}" ]
 then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 ln -sf ./coberly-gruvbox.zsh-theme ~/.oh-my-zsh/custom/themes
+chsh -s /bin/zsh $USERNAME
 
-#Remove old dotfiles and replace them for the host machine
-#DOTFILES=(.gitconfig .bashrc .zshrc .aliases init.vim .tmux.conf, .tmux.conf.local) 
-#for dotfile in $(echo ${DOTFILES[*]});
-#do
-#  sudo rm -rf ~/$(echo $dotfile)
-#  ln -sf $(echo $dotfile) ~/$(echo $dotfile)
-#done
+# Programs
+
+sudo apt update
+
+## Docker
+
+sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+## Utilities
+
+sudo apt install ranger htop ncdu nmap p7zip peek powertop tldr tree unrar whereami zsh wget curl
+
+## Development
+
+sudo apt install tmux neovim fonts-powerline gh git heroku postgresql
