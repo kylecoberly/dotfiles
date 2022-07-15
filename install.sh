e1d57d1c231eb0156951bb214149eb92d305dde3
Kyle Coberly <kyle.coberly@gmail.com>
2022-07-15 14:09:21 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,9 @@ sudo apt install -y --no-install-recommends tmux neovim
 ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/coberly-gruvbox.zsh-theme $HOME/.oh-my-zsh/custom/themes
 mkdir -p $HOME/.config/nvim && ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/init.vim $HOME/.config/nvim
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-DOTFILES=(.gitconfig .zshrc .aliases .tmux.conf, .tmux.conf.local) 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+DOTFILES=(.gitconfig .zshrc .aliases .tmux.conf .tmux.conf.local) 
 for dotfile in $(echo ${DOTFILES[*]});
 do
   ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/$(echo $dotfile) $HOME/$(echo $dotfile)
