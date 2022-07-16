1fa551403fc3e45f8997ca2d89018e9ddfb08646
Kyle Coberly <kyle.coberly@gmail.com>
2022-07-15 19:49:55 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install node
-python3 -m pip pynvim
+python3 -m pip install --upgrade pynvim
 # pip install pynvim
 
 ## Link Files
