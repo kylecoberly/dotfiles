f654250647bab556b1e1f6c831b94ebae4c71014
Kyle Coberly <kyle.coberly@gmail.com>
2023-04-04 02:53:20 -0600

    Add alias for code action

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -31,6 +31,7 @@ local on_attach = function(_, bufnr)
 
   nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
   nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+  nmap("<leader>do", vim.lsp.buf.code_action, "'do', alias for: [C]ode [A]ction")
 
   nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
   nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
