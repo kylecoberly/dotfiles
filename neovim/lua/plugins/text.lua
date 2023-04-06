0dc6d1eca89080d541efdf6de11edd1e7777df03
Kyle Coberly <kyle.coberly@gmail.com>
2023-04-06 13:04:57 -0600

    Add more plugins

---
+++ neovim/lua/plugins/text.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "jghauser/follow-md-links.nvim",
+    keymaps = {
+      { "n", "<BS>", ":edit #<CR>" },
+    },
+  },
+  {
+    "Pocco81/true-zen.nvim",
+    config = function()
+      local truezen = require("true-zen")
+      truezen.setup({
+        integrations = {
+          tmux = true,
+          lualine = true,
+        },
+      })
+
+      vim.keymap.set("n", "<Leader>m", function()
+        truezen.ataraxis()
+      end, {
+        desc = "Zen: Ataraxis",
+        noremap = true,
+      })
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    config = function()
+      vim.fn["mkdp#util#install"]()
+      vim.g.mkdp_port = 8080
+      vim.g.mkdp_auto_close = 0
+      vim.g.mkdp_echo_preview_url = 1
+
+      vim.keymap.set("n", "<Leader>p", ":MarkdownPreviewToggle<CR>", {
+        desc = "Toggle live markdown preview",
+      })
+    end,
+  },
+}
---
+++ neovim/lua/plugins/theme.lua
@@ -0,0 +1,16 @@
+return {
+  { "sainnhe/gruvbox-material" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      theme = "gruvbox-material",
+    },
+  },
+}
