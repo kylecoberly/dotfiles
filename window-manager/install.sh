b8bf75d5829c3f7ab95546499ebce1b2b680c8cc
Kyle Coberly <kylecoberly@Kyles-Mac-mini.local>
2023-04-26 01:51:42 -0600

    Update window management

--- window-manager/install.sh
+++ window-manager/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# IMPORTANT: For MacOS, disable SIP first: https://github.com/koekeishiya/yabai/wiki/Disabling-System-Integrity-Protection
+# On Intel: Hold command-R while booting. Go to "Utilities" → "Terminal" to open bash, then run `csrutil disable --with kext --with dtrace --with nvram --with basesystem`. Reboot.
+# After reboot, run `sudo visudo -f /private/etc/sudoers.d/yabai` to add Yabai to sudoers and add the following line:
+# <output of `whoami`> ALL=(root) NOPASSWD: sha256:<output of `shasum -a 256 $(which yabai)`> <output of `which yabai`> --load-sa
+# For example:
+# kylecoberly ALL=(root) NOPASSWD: sha256:740b9e6aab46f8c499f0fc651ae1861d4ebe48b6e6a50296bf4a9ad879bbad93 /usr/local/bin/yabai --load-sa
+
 # Requires Brew
 
 if [ -z "$HOME" ]; then
@@ -21,11 +28,15 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 	DOTFILES=(.yabairc .skhdrc .spacebarrc)
 	for dotfile in "${DOTFILES[@]}"; do
 		rm "${HOME}/${dotfile}"
-		ln -sf "${DOTFILE_DIRECTORY}/apps/${dotfile}" "${HOME}/"
+		ln -sf "${DOTFILE_DIRECTORY}/window-manager/${dotfile}" "${HOME}/"
 	done
+
 	brew install koekeishiya/formulae/yabai
 	brew services start yabai
 
 	brew install koekeishiya/formulae/skhd
 	brew services start skhd
+
+	brew install cmacrae/formulae/spacebar
+	brew services start spacebar
 fi
