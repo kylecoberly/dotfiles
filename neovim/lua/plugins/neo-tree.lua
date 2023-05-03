ec2f4fea6d4a79afdf2c0a81d11ca3cb6c9653c4
Kyle Coberly <kyle.coberly@gmail.com>
2023-05-03 02:03:08 -0600

    Adding more config to the tree

--- neovim/lua/plugins/neo-tree.lua
+++ neovim/lua/plugins/neo-tree.lua
@@ -10,4 +10,24 @@ return {
       remap = true,
     },
   },
+  opts = {
+    window = {
+      filesystem = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+      follow_current_file = true,
+      window = {
+        mappings = {
+          ["-"] = "navigate_up",
+          ["."] = "set_root",
+          ["/"] = "fuzzy_finder",
+        },
+        fuzzy_finder_mappings = {
+          ["<C-n>"] = "move_cursor_down",
+          ["<C-N>"] = "move_cursor_up",
+        },
+      },
+    },
+  },
 }
