a10798255ce053192ba21e2d9a9fbe480fd0338f
Kyle Coberly <kyle.coberly@gmail.com>
2023-05-03 23:21:47 -0600

    WIP i3

--- window-manager/ubuntu/i3/scripts/i3-screenlayout.sh
+++ window-manager/ubuntu/i3/scripts/i3-screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output XWAYLAND0 --off --output XWAYLAND1 --primary --mode 3840x2160 --pos 0x0 --rotate normal
+xrandr --output XWAYLAND0 --off --output XWAYLAND1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
