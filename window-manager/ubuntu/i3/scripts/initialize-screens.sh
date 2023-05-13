70768b42e902706d380d0fc7048a1c4ed63e55d0
Kyle Coberly <kyle.coberly@gmail.com>
2023-05-13 03:23:42 -0600

    WIP i3

--- window-manager/ubuntu/i3/scripts/initialize-screens.sh
+++ window-manager/ubuntu/i3/scripts/initialize-screens.sh
@@ -2,13 +2,14 @@
 
 # See names and resolutions of attached screens by running `xrandr`
 
-LAPTOP_SCREEN="eDP-1"
-PRIMARY_SCREEN="DP-2"
-SECONDARY_SCREEN="DP-1"
+LAPTOP_SCREEN="eDP1"
+PRIMARY_SCREEN="DP2"
+SECONDARY_SCREEN="DP1"
 
 xrandr \
 	--output $PRIMARY_SCREEN --primary --mode 1920x1080 --rotate normal \
 	--output $LAPTOP_SCREEN --off
 # --output $LAPTOP_SCREEN --mode 1280x720
+# --output $PRIMARY_SCREEN --off \
 # --output $LAPTOP_SCREEN --mode 1280x720 --rotate normal --below $PRIMARY_SCREEN
 # --output $SECONDARY_SCREEN --mode 1280x720 --rotate left --right-of $PRIMARY_SCREEN
