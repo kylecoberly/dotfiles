697f22579ac94e92a85e6d3dc5e637fcbc1aa828
Kyle Coberly <kyle.coberly@gmail.com>
2023-09-25 23:38:26 -0600

    Update docker install

--- apps/install.sh
+++ apps/install.sh
@@ -16,10 +16,23 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	## Install Docker
+	sudo apt-get update
+	sudo apt-get install ca-certificates curl gnupg
+	sudo install -m 0755 -d /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	sudo chmod a+r /etc/apt/keyrings/docker.gpg
+	echo \
+		"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+		"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+		sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+	sudo apt-get update
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+	## Install apps
 	sudo apt-get install -y --no-install-recommends \
 		ranger bat htop ncdu nmap tldr tree wget ffmpeg \
-		openssh-server xclip build-essential docker-ce docker-ce-cli \
-		p7zip rar unrar
+		openssh-server xclip build-essential p7zip rar unrar
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	brew install ranger bat htop ncdu nmap tldr tree wget ffmpeg \
 		openssh-server docker xsel rar
