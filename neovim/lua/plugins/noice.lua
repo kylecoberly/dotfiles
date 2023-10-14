2ed7e72ae4f4c88ca0c2d8e36e11036e690f8b2b
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-14 14:34:10 -0600

    Shh up the lsp messages

--- neovim/lua/plugins/noice.lua
+++ neovim/lua/plugins/noice.lua
@@ -1,3 +1,47 @@
+-- Quiet down ltex, etc.
+local null_ls_token = nil
+local ltex_token = nil
+vim.lsp.handlers["$/progress"] = function(_, result, ctx)
+  local value = result.value
+  if not value.kind then
+    return
+  end
+
+  local client_id = ctx.client_id
+  local name = vim.lsp.get_client_by_id(client_id).name
+
+  if name == "null-ls" then
+    if result.token == null_ls_token then
+      return
+    end
+    if value.title == "formatting" then
+      null_ls_token = result.token
+      return
+    end
+    if value.title == "diagnostics" then
+      null_ls_token = result.token
+      return
+    end
+  end
+  if name == "info" then
+    return
+  end
+
+  if name == "ltex" then
+    if result.token == ltex_token then
+      return
+    end
+    if value.title == "Checking document" then
+      ltex_token = result.token
+      return
+    end
+  end
+
+  vim.notify(value.message, "info", {
+    title = value.title,
+  })
+end
+
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
@@ -9,6 +53,11 @@ return {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
         ["cmp.entry.get_documentation"] = true,
+      },
+      {
+        progress = {
+          enabled = false,
+        },
       },
     },
     presets = {
