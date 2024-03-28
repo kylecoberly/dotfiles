5c5d5ab4b83a99d0a911d9162c15765cf1145775
Kyle Coberly <kyle.coberly@gmail.com>
2024-03-28 16:08:13 -0600

    Add user config

--- astronvim/nvim/lua/user/init.lua
+++ astronvim/nvim/lua/user/init.lua
@@ -1,6 +1,67 @@
-vim.opt.termguicolors = true
-vim.cmd.colorscheme 'melange'
-
 return {
-  colorscheme = "melange"
+  colorscheme = "melange",
+  mappings = {
+    n = {
+      ["<Leader>m"] = { "<cmd>ZenMode<cr>" }
+    },
+  },
+  plugins = {
+    {
+      import = "astrocommunity.colorscheme.melange-nvim",
+    },
+    {
+      import = "astrocommunity.pack.typescript-all-in-one",
+    },
+    {
+      import = "astrocommunity.diagnostics.lsp_lines-nvim",
+    },
+    {
+      import = "astrocommunity.diagnostics.trouble-nvim",
+    },
+    {
+      import = "astrocommunity.editing-support.telescope-undo-nvim",
+    },
+    {
+      import = "astrocommunity.editing-support.zen-mode-nvim",
+    },
+    {
+      "nvim-autopairs",
+      enabled = false,
+    },
+    {
+      "akinsho/toggleterm.nvim",
+      version = "false",
+      opts = {
+        open_mapping = [[<Leader>t]]
+        direction = "tab"
+      }
+    },
+    {
+      "kylechui/nvim-surround",
+      version = "*",
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({})
+      end
+    },
+    {
+      "chentoast/marks.nvim",
+    },
+    {
+      "aserowy/tmux.nvim",
+    },
+    {
+      "rmagatti/auto-session",
+    },
+    {
+    "toppair/peek.nvim",
+      event = { "VeryLazy" },
+      build = "deno task --quiet build:fast",
+      config = function()
+        require("peek").setup()
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+      end,
+    },
+  }
 }
