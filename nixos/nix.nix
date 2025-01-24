05dcc04a3b9608e19e4339937701156d3420dd5d
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-24 02:36:05 -0700

    Fix error

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -20,5 +20,5 @@
     # Before changing this value read the documentation for this option
     # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
     system.stateVersion = "24.11"; # Did you read the comment?
-  }
+  };
 }
