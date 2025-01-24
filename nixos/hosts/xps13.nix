9c5eda8094720f2f9b28eef6f8683794f51aff25
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-24 04:01:50 -0700

    Fix error

--- nixos/hosts/xps13.nix
+++ nixos/hosts/xps13.nix
@@ -1,11 +1,52 @@
 { inputs, lib, config, pkgs, ... }: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
-    ./common.nix
     ../hardware-configuration.nix
     ../desktops/gnome.nix
-    ../home/kylecoberly.nix
   ];
+
+  ## Bootloader
+  boot = {
+    loader = {
+      grub.enable = true;
+      grub.device = "/dev/vda";
+      grub.useOSProber = true;
+    };
+    kernel.sysctl = { "vm.swappiness" = 10;};
+  };
+
+  ## Networking
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  ## Audio
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  ## Peripherals
+  services.printing.enable = true;
+
+  ## Locale
+  time.timeZone = "America/Denver";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
 
   inputs.home-manager = {
     backupFileExtension = "back";
@@ -18,4 +59,42 @@
 
   environment.systemPackages = with pkgs; [
   ];
+
+  ## User
+  inputs.home-manager.users.kylecoberly = {
+    isNormalUser = true;
+    description = "Kyle Coberly";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      chromium
+      obsidian
+      rambox
+      neovim
+      obs-studio
+      docker
+      # Languages
+      # asdf-vm
+      # perl
+      # ruby
+      # python3
+      # php
+      # go
+      # rustc
+      # deno
+      # luarocks
+      # html-tidy
+    ];
+  };
+
+  programs = {
+    tmux = {
+      enable = true;
+      clock24 = true;
+      keyMode = "vi";
+      extraConfig = "mouse on";
+    };
+    git = {
+      enable = true;
+    };
+  };
 }
