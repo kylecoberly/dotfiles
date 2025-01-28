959da27f03676824041e1cf04283aee56d8f53c6
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-28 02:36:18 -0700

    Fix error

--- nixos/apps/dotfiles/default.nix
+++ nixos/apps/dotfiles/default.nix
@@ -5,7 +5,7 @@
     enable = true;
     configFile = {
       "git/config" = {
-        source = config.mkOutOfStoreSymLink "${config.home.homeDirectory}/dotfiles/nixos/apps/dotfiles/.gitconfig";
+        source = config.lib.file.mkOutOfStoreSymLink "${config.home.homeDirectory}/dotfiles/nixos/apps/dotfiles/.gitconfig";
       };
     };
   };
