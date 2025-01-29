4812e805f611a695e6cdec92ff71b3e7494a66f5
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-29 03:09:50 -0700

    Fix error

--- nixos/apps/git/default.nix
+++ nixos/apps/git/default.nix
@@ -1,11 +1,11 @@
-{ config, lib, pkgs, osConfig, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  osConfig.programs.git.enable = true;
-  osConfig.xdg.configFile = {
+  programs.git.enable = true;
+  xdg.configFile = {
     "git/config".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/apps/git/.gitconfig";
   };
-  osConfig.home.file = {
+  home.file = {
     ".gitignore".source  = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/apps/git/.gitignore";
   };
 }
