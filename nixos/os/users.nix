3a0629cf9f95b57a5282cad77b3c55624eca4af2
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-29 03:44:23 -0700

    Fix

--- nixos/os/users.nix
+++ nixos/os/users.nix
@@ -1,19 +1,21 @@
 { config, lib, pkgs, ... }:
 
 {
-  users.users = {
-   kylecoberly = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      ## initialPassword = "correcthorsebatterystaple";
-      isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      ];
-      extraGroups = ["networkmanager docker wheel"];
-    };
+  users = { 
     defaultUserShell = pkgs.zsh;
+    users = {
+      kylecoberly = {
+        # TODO: You can set an initial password for your user.
+        # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
+        # Be sure to change it (using passwd) after rebooting!
+        ## initialPassword = "correcthorsebatterystaple";
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+        ];
+        extraGroups = ["networkmanager docker wheel"];
+      };
+    };
   };
 
   environment.pathsToLink = [ "/share/zsh" ];
