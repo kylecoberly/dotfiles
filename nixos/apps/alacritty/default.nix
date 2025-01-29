f79c2b7cfc488955a591283bd74669beb0dd1d6c
Kyle Coberly <kyle.coberly@gmail.com>
2025-01-29 02:00:15 -0700

    Fix error

--- nixos/apps/alacritty/default.nix
+++ nixos/apps/alacritty/default.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  config.programs.alacritty = enable;
+  config.programs.alacritty.enable = true;
   config.xdg = {
     configFile."alacritty/alacritty.toml".source = "../../apps/alacritty/alacritty.toml";
     configFile."alacritty/melange.toml".source = "../../apps/alacritty/melange.toml";
