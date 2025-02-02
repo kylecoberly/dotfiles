1dc0f472eae388991fe1d43affa8ceca1a9fe5d8
Kyle Coberly <kyle.coberly@gmail.com>
2025-02-02 02:23:04 -0700

    Fix

--- nixos/os/users.nix
+++ nixos/os/users.nix
@@ -1,8 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
+  programs.zsh.enable = true;
   users.users.kylecoberly = {
     isNormalUser = true;
     shell = pkgs.zsh;
+    extraGroups = [ "wheel" ];
   };
 }
