312372f625509aadda417e3c84c823c6fe705c31
Kyle Coberly <kyle.coberly@gmail.com>
2025-02-02 03:03:32 -0700

    Fix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -5,6 +5,17 @@
     ./systems/xps13
     ./os
   ];
+  environment.etc = {
+    # somerc.source = /etc/somerc;
+    "tmux/tmux.conf" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/dotfiles/tmux/tmux.conf";
+      recursive = true;
+    };
+    "tmux/tmux.conf.local" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/dotfiles/tmux/tmux.conf.local";
+      recursive = true;
+    };
+  };
 
   nix = {
     gc = {
--- nixos/dotfiles/tmux/default.nix
+++ nixos/dotfiles/tmux/default.nix
@@ -1,17 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  config.environment.etc = {
-    # somerc.source = /etc/somerc;
-    "tmux/tmux.conf" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/dotfiles/tmux/tmux.conf";
-      recursive = true;
-    };
-    "tmux/tmux.conf.local" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/dotfiles/tmux/tmux.conf.local";
-      recursive = true;
-    };
-  };
   # xdg.configFile = {
   # };
 
