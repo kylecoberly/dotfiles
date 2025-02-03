cf20099fb3b3b747c4e2ca8403b3da866800c963
Kyle Coberly <kyle.coberly@gmail.com>
2025-02-03 01:28:23 -0700

    Remove dotfiles

--- nixos/home/directories.nix
+++ nixos/home/directories.nix
@@ -1,13 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.file."dotfiles" = {
-    source = builtins.fetchGit {
-      url = "https://github.com/kylecoberly/dotfiles.git";
-      ref = "refs/heads/master";
-    };
-    recursive = true;
-  };
   xdg.userDirs.templates = null;
   xdg.userDirs.music = null;
   xdg.userDirs.pictures = null;
