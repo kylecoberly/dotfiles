090b91f486795109981881ed450c7fd09116c374
Kyle Coberly <kyle.coberly@gmail.com>
2025-02-03 03:41:21 -0700

    Fix

--- nixos/dotfiles/obsidian/default.nix
+++ nixos/dotfiles/obsidian/default.nix
@@ -2,7 +2,6 @@
 
 {
   xdg = {
-    # configFile."obsidian/obsidian.json".source = "${config.home.homeDirectory}/dotfiles/nixos/dotfiles/obsidian/obsidian.json";
-    configFile."obsidian/Preferences".source = "${config.home.homeDirectory}/dotfiles/nixos/dotfiles/obsidian/Preferences";
+    configFile."obsidian/obsidian.json".source = "${config.home.homeDirectory}/dotfiles/nixos/dotfiles/obsidian/obsidian.json";
   };
 }
