ea960c7270b6e08d5ed3d5ceb4abe9bfdb90ccf7
Kyle Coberly <kyle.coberly@gmail.com>
2025-02-03 03:43:52 -0700

    FIx

--- nixos/dotfiles/obsidian/default.nix
+++ nixos/dotfiles/obsidian/default.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  xdg = {
-    # configFile."obsidian/obsidian.json".source = "${config.home.homeDirectory}/dotfiles/nixos/dotfiles/obsidian/obsidian.json";
+  xdg.configFile = {
+    "obsidian/obsidian.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/dotfiles/obsidian/obsidian.json";
   };
 }
