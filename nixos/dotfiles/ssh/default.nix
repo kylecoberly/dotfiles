586e6984b366c3b2ed84a16f29ed65e36ebe0eb0
Kyle Coberly <kyle.coberly@gmail.com>
2025-02-03 03:15:43 -0700

    Fix path

--- nixos/dotfiles/ssh/default.nix
+++ nixos/dotfiles/ssh/default.nix
@@ -2,7 +2,7 @@
 
 let
   # Mac: onePassPath = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
-  onePassPath = "~/.1password/agent.sock";
+  onePassPath = "${config.home.homeDirectory}/.1password/agent.sock";
 in {
   programs.ssh = {
     enable = true;
