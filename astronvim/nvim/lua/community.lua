06454d89e12ff583c51e3e507dfae7f7aaf00add
Kyle Coberly <kyle.coberly@gmail.com>
2025-02-04 13:11:25 -0700

    Update astronvim

---
+++ astronvim/nvim/lua/community.lua
@@ -0,0 +1,34 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.colorscheme.melange-nvim" },
+    { import = "astrocommunity.pack.typescript" },
+    { import = "astrocommunity.diagnostics.trouble-nvim" },
+    { import = "astrocommunity.editing-support.telescope-undo-nvim" },
+    { import = "astrocommunity.editing-support.zen-mode-nvim" },
+    { import = "astrocommunity.motion.flash-nvim" },
+    { import = "astrocommunity.motion.marks-nvim" },
+    { import = "astrocommunity.motion.nvim-surround" },
+    { import = "astrocommunity.scrolling.mini-animate" },
+    { import = "astrocommunity.search.nvim-spectre" },
+    { import = "astrocommunity.markdown-and-latex.peek-nvim" },
+    { import = "astrocommunity.git.blame-nvim" },
+    { import = "astrocommunity.completion.tabnine-nvim" },
+  },
+  {
+    "nvim-autopairs",
+    enabled = false,
+  },
+  {
+    "aserowy/tmux.nvim",
+  },
+  {
+    "rmagatti/auto-session",
+  },
+}
--- astronvim/nvim/lua/plugins/mason.lua
+++ astronvim/nvim/lua/plugins/mason.lua
@@ -1,25 +1,39 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Mason plugins
+
+---@type LazySpec
 return {
+  -- use mason-lspconfig to configure LSP installations
   {
-    "williamboman/mason.nvim",
-    cmd = {
-      "Mason",
-      "MasonInstall",
-      "MasonUninstall",
-      "MasonUninstallAll",
-      "MasonLog",
-      "MasonUpdate", -- AstroNvim extension here as well
-      "MasonUpdateAll", -- AstroNvim specific
-    },
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_uninstalled = "✗",
-          package_pending = "⟳",
-        },
+      ensure_installed = {
+        "lua_ls",
+        -- add more arguments for adding more language servers
       },
     },
-    build = ":MasonUpdate",
-    config = require "plugins.configs.mason",
+  },
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = {
+      ensure_installed = {
+        "stylua",
+        -- add more arguments for adding more null-ls sources
+      },
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    opts = {
+      ensure_installed = {
+        "python",
+        -- add more arguments for adding more debuggers
+      },
+    },
   },
 }
